cf7427ecbab1311e3257bb85b8e601f4f4c17dd5
Oscar Ryley <oscar.ryley@gmail.com>
2023-11-04 20:38:03 +0000

    Updated app.js
    Can now change text within the information boxes via the Button clicks

---
+++ app.js
@@ -0,0 +1,47 @@
+function changeTextOne() {
+    MainTitle.innerText = "Title of Section";
+    Title1.innerText =  "Title for box 1";
+    Text1.innerText =  "Text for box 1";
+    Title2.innerText =  "Title for box 2";
+    Text2.innerText =  "Text for box 2";
+    Title3.innerText =  "Title for box 3";
+    Text3.innerText =  "Text for box 3";
+    Title4.innerText =  "Title for box 4";
+    Text4.innerText =  "Text for box 4";
+}
+
+function changeTextTwo() {
+    MainTitle.innerText = "Title of Section";
+    Title1.innerText =  "Title for box 1";
+    Text1.innerText =  "Text for box 1";
+    Title2.innerText =  "Title for box 2";
+    Text2.innerText =  "Text for box 2";
+    Title3.innerText =  "Title for box 3";
+    Text3.innerText =  "Text for box 3";
+    Title4.innerText =  "Title for box 4";
+    Text4.innerText =  "Text for box 4";
+}
+
+function changeTextThree() {
+    MainTitle.innerText = "Title of Section";
+    Title1.innerText =  "Title for box 1";
+    Text1.innerText =  "Text for box 1";
+    Title2.innerText =  "Title for box 2";
+    Text2.innerText =  "Text for box 2";
+    Title3.innerText =  "Title for box 3";
+    Text3.innerText =  "Text for box 3";
+    Title4.innerText =  "Title for box 4";
+    Text4.innerText =  "Text for box 4";
+}
+
+function changeTextFour() {
+    MainTitle.innerText = "Title of Section";
+    Title1.innerText =  "Title for box 1";
+    Text1.innerText =  "Text for box 1";
+    Title2.innerText =  "Title for box 2";
+    Text2.innerText =  "Text for box 2";
+    Title3.innerText =  "Title for box 3";
+    Text3.innerText =  "Text for box 3";
+    Title4.innerText =  "Title for box 4";
+    Text4.innerText =  "Text for box 4";
+}
